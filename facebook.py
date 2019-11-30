9a6322d56d3d987a1e65e24af89638e903b1f460
stevinalex <37404565+stevinalex@users.noreply.github.com>
2019-12-01 03:41:42 +0630

    Facebook.py
    fbhack

---
+++ facebook.py
@@ -0,0 +1,51 @@
+# mmhk
+Fbhack
+import sys
+import os
+import time
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year 
+os.system("apt install figlet")
+os.system("clear")
+os.system("Stevin Alex FB Hack")
+time.sleep(5)
+if sys.version_info[0] !=2:
+print('''---Lee Ko FB Hack Chin Tr Lr 0%---''')
+time.sleep(3)
+if sys.version_info[0] !=2: 	
+print('''---Lee Bel Ya Mel 10%---''')
+time.sleep(3)
+if sys.version_info[0] !=2: 	
+print('''---Min May Sout Phot Ko Hack 20%---''')
+time.sleep(3)
+if sys.version_info[0] !=2: 	
+print('''---Hack Min May Sout Phot 30%---''')
+ltime.sleep(3)
+if sys.version_info[0] !=2: 	
+print('''---Hack Min May Sout Phot 40%---''')
+time.sleep(3)
+if sys.version_info[0] !=2: 	
+print('''---Hack Min May Sout Phot 50%---''')
+time.sleep(3)
+if sys.version_info[0] !=2: 	
+print('''---Hack Min May Sout Phot 60%---''')
+time.sleep(3)
+if sys.version_info[0] !=2: 	
+print('''---Hack Min May Sout Phot 70%---''')
+time.sleep(3)
+if sys.version_info[0] !=2: 	
+print('''---Hack Min May Sout Phot 80%---''')
+time.sleep(3)
+if sys.version_info[0] !=2:	
+print('''---Hack Min May Sout Phot 90%---''')
+time.sleep(3)
+if sys.version_info[0] !=2: 	
+print('''---Hack Min May Sout Phote 100%---''')
+if sys.version_info[0] !=2:	
+print('''Min May Sout Phot is Hacked By MMHK Hacking Knowledge---''') 
